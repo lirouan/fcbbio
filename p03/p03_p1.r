8f748abb4415b9602a7a317e3a3632d0cdac5161
Joanne Li <jli460@jh.edu>
2026-02-21 11:59:37 -0500

    modified p03

---
+++ p03/p03_p1.r
@@ -0,0 +1,11 @@
+# set seed for reproducibility
+set.seed(3)
+
+# load required libraries
+suppressPackageStartupMessages({
+    library(ROCR) # ROC curves and AUC
+    library(kernlab) # SPAM dataset
+})
+
+
+
